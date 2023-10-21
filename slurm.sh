fc9d4f0423c404d2144006ef90dde6eb7b39f333
priyanshul-govil <priyanshul.amity9i@gmail.com>
2023-10-21 05:59:24 -0400

    .

--- slurm.sh
+++
@@ -1,5 +0,0 @@
-for model in $(cat models.txt); do
-    sbatch response.slurm $model
-done
-
-# cd "/home/<username>/<folder>/llm-consistency/data/responses/" && for model in $(cat /home/<username>/<folder>/llm-consistency/models.txt); do awk 'NR==1{print $0;next} FNR>1' "/home/<username>/<folder>/llm-consistency/data/responses/$model"* > "/home/<username>/<folder>/llm-consistency/data/responses/$model.csv"; done 
